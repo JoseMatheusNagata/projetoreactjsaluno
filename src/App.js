796603556724f1e2e26912e5770c6677df12955e
José Matheus Nagata Kulibaba <62066792+JoseMatheusNagata@users.noreply.github.com>
2025-03-23 17:49:06 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import OficinaComponent from './Components/OficinaComponent';
 function App() {
   return (
     <div className="App">
      <h1> Projeto para o aluno praticar </h1>
+         <OficinaComponent />
     </div>
   );
 }
---
+++ src/Components/OficinaComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OficinaComponent = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Oficina</h1>
+      <p>Este é o componente OficinaComponent.</p>
+    </div>
+  );
+};
+
+export default OficinaComponent;
