663422a37df34d6d2eb555154eef92087b2974b4
José Matheus Nagata Kulibaba <josematheusnagatak@hotmail.com>
2025-04-12 20:48:26 -0300

    ajusted rodaPeComponent and menu css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Menu from './Components/Menu';
 function App() {
   return (
     <div className="App">
-     <h1></h1>
+     <h1>Welcome to My App</h1>
          <Menu />
          <RodaPeComponent/>
     </div>
--- src/Components/RodaPeComponent.js
+++ src/Components/RodaPeComponent.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import './RodaPeComponent.css'; // Opcional: para estilização
+import React from "react";
+import "./RodaPeComponent.css";
 
 const RodaPeComponent = () => {
-    return (
-        <footer>
-            <p>&copy; {new Date().getFullYear()} Minha Aplicação. Todos os direitos reservados.</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Minha Aplicação. Todos os direitos
+        reservados.
+      </p>
+
+      <p>Contato: contato@meusite.com | Telefone: (11) 1234-5678</p>
+      <div className="social-links">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+      </div>
+    </footer>
+  );
 };
-
-
 
 export default RodaPeComponent;
